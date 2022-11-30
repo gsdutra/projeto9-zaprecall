879c587978f26e8ef6d287d5c2ac44d90deca546
dutra-86 <gabrielsprecherdutra@gmail.com>
2022-11-30 12:25:27 -0300

    Implementada tela de boas vindas parcialmente

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import BoasVindas from './BoasVindas.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const [telaBoasVindas, setBV] = React.useState(true);
+
+	function exitBoasVindas(){
+		setBV(false);
+	}
+
+	return (
+		<BoasVindas func={exitBoasVindas}/>
+	);
 }
 
 export default App;
---
+++ src/BoasVindas.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import styled from 'styled-components';
+import logo from './assets/img/logo.png';
+
+export default function BoasVindas(props){
+    return(
+        <Content>
+            <Centered>
+                <img src={logo}/>
+                <Text>ZapRecall</Text>
+                <Butt onClick={props.func}>Iniciar Recall!</Butt>
+            </Centered>
+        </Content>
+    )
+}
+
+const Content = styled.div`
+    width: 100%;
+    height: 100vh;
+    background-color: #FB6B6B;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+const Centered = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+const Text = styled.div`
+    color: white;
+    margin-top: 13px;
+
+    font-family: Righteous;
+    font-size: 36px;
+    font-weight: 400;
+    line-height: 45px;
+    letter-spacing: -0.012em;
+    text-align: center;
+
+`
+const Butt = styled.div`
+    margin-top: 30px;
+    font-family: Recursive;
+
+    font-size: 18px;
+    font-weight: 400;
+    line-height: 22px;
+    letter-spacing: 0em;
+    text-align: center;
+
+    background: #FFFFFF;
+    color: #D70900;
+    border-radius: 5px;
+    border: 1px solid #D70900;
+    padding-top: 16px;
+    padding-bottom: 16px;
+    width: 246px;
+    transition: .3s;
+
+    &:hover{
+        cursor: pointer;
+        transform: scale(1.05);
+        transition: .3s;
+    }
+
+`
